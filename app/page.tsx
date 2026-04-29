3c566053332fe1ce3b7e13c6cd504e320a915354
Swaroop Jadhav <swaroopjadhav5@gmail.com>
2026-04-29 10:55:20 +0530

    Integrate CustomCursor for global fluid pointer tracking

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
 import ScrollProgress from "@/components/ScrollProgress";
+import CustomCursor from "@/components/CustomCursor";
 
 export default function Home() {
   return (
     <main>
+      <CustomCursor />
       <ScrollProgress />
       <Navbar />
       <div
