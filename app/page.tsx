cd874c5f07ed050fa2bc018d86372014229fcaa3
Swaroop Jadhav <swaroopjadhav5@gmail.com>
2026-04-30 10:30:45 +0530

    Mount Intro boot animation to root landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import ScrollProgress from "@/components/ScrollProgress";
 import CustomCursor from "@/components/CustomCursor";
+import Intro from "@/components/Intro";
 
 export default function Home() {
+  const [introComplete, setIntroComplete] = useState(false);
+
   return (
     <main>
       <CustomCursor />
       <ScrollProgress />
-      <Navbar />
+      <Intro onComplete={() => setIntroComplete(true)} />
+      {introComplete && <Navbar />}
       <div
         style={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           height: "100vh",
+          fontFamily: "monospace",
           color: "#ff5500",
           fontSize: "20px",
         }}
