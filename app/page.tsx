301342135af2801df511d11cb50527659d6363bb
Swaroop Jadhav <swaroopjadhav5@gmail.com>
2026-05-02 18:15:42 +0530

    Fix double rendering glitch caused by Intro state update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Navbar from "@/components/Navbar";
 import ScrollProgress from "@/components/ScrollProgress";
 import CustomCursor from "@/components/CustomCursor";
@@ -10,17 +10,17 @@ import Hero from "@/components/Hero";
 export default function Home() {
   const [introComplete, setIntroComplete] = useState(false);
 
+  const handleIntroComplete = useCallback(() => {
+    setIntroComplete(true);
+  }, []);
+
   return (
     <main>
       <CustomCursor />
       <ScrollProgress />
-      <Intro onComplete={() => setIntroComplete(true)} />
-      {introComplete && (
-        <>
-          <Navbar />
-          <Hero />
-        </>
-      )}
+      {!introComplete && <Intro onComplete={handleIntroComplete} />}
+      <Navbar />
+      <Hero />
     </main>
   );
 }
