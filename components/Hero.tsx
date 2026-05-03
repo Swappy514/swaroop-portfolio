0a58e864d704ffac31b0dd44f242abc458941eeb
Swaroop Jadhav <swaroopjadhav5@gmail.com>
2026-05-03 12:05:15 +0530

    Adjust background orbit ring dimensions to match new layout bounds

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -156,7 +156,7 @@ export default function Hero() {
         }}
       >
         {/* Orbit rings */}
-        {[480, 580].map((size, i) => (
+        {[460, 580].map((size, i) => (
           <div
             key={size}
             style={{
