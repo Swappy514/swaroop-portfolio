d104accf22ceb6bf2812026fc1ca032cdfb431bf
Swaroop Jadhav <swaroopjadhav5@gmail.com>
2026-05-05 09:45:22 +0530

    Refine Hero center container with fluid clamp width to prevent text wrapping

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -182,7 +182,7 @@ export default function Hero() {
             position: "relative",
             zIndex: 10,
             textAlign: "center",
-            width: "300px",
+            width: "clamp(280px, 40vw, 480px)",
           }}
         >
           <h1
