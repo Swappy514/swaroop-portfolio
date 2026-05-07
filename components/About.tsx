4db9dcb6cf3700d18138b6ff83c2a673076be5ca
Swaroop Jadhav <swaroopjadhav5@gmail.com>
2026-05-07 11:48:22 +0530

    Upgrade social links to interactive icon row with staggered hover states

--- components/About.tsx
+++ components/About.tsx
@@ -340,7 +340,7 @@ export default function About() {
         </div>
 
         {/* Bottom — Social Links */}
-        <div style={{ position: "relative", zIndex: 2 }}>
+        <div style={{ position: "relative", zIndex: 1 }}>
           <div
             style={{
               fontFamily: "var(--font-inter)",
